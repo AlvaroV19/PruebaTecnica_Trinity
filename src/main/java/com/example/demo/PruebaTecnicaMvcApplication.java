5c70edc6faf09572fd0b52b73c3c5e975f27335c
AlvaroV19 <afavila-2022a@corhuila.edu.co>
2026-02-12 17:07:52 -0500

    Initial structure MVC

---
+++ src/main/java/com/example/demo/PruebaTecnicaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaMvcApplication.class, args);
+	}
+
+}
